ebf268a62e6ca5b83ada68b086fec2169fb6ef27
lixianghua <lxh@fuhengkeji.com>
2018-12-11 17:12:44 +0800

    rocketmq消息队列

---
+++ src/main/java/com/rocketmq/Consumer.java
@@ -0,0 +1,55 @@
+package com.rocketmq;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author 李祥华
+ * @description
+ * @time 2018/12/6 15:29
+ */
+public class Consumer {
+
+    public static void main(String[] args) throws Exception {
+        //新建一个消费对象
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("consumer_group");
+
+        consumer.setNamesrvAddr("127.0.0.1:9876");
+
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+
+        consumer.subscribe("topic_example_java","*");
+
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+                //默认 list 里只有一条消息，可以通过设置参数来批量接收消息
+                if (list != null) {
+                    for (MessageExt ext : list) {
+                        try {
+                            System.out.println(new Date() + new String(ext.getBody(), RemotingHelper.DEFAULT_CHARSET));
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        consumer.start();
+        System.out.println("-------消费者已经启动");
+
+    }
+}
